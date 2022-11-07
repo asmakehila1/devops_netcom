c3066f6e855a56240cf7029765109e73a198623a
levy20 <levy.bedi@esprit.tn>
2022-11-07 01:53:47 +0100

    Test Junit

---
+++ tpAchatProject/src/test/java/com/esprit/examen/services/ReglementServicelmplTest.java
@@ -0,0 +1,58 @@
+package com.esprit.examen.services;
+
+
+import static org.junit.Assert.*;
+import java.util.List;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import com.esprit.examen.entities.Reglement;
+
+
+
+import java.text.ParseException;
+
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReglementServicelmplTest {
+	@Autowired
+	IReglementService reglementService;
+	
+	@Test
+	public void testAddReglement() {
+		//	List<Reglement> = reglementService.retrieveAllReglements();
+		//	int expected=reglement.size();
+		Reglement r = new Reglement();
+			Reglement savedReglement= reglementService.addReglement(r);
+			
+		//	assertEquals(expected+1, reglementServic.retrieveAllReglements().size());
+			assertNotNull(savedReglement.getIdReglement());
+			reglementService.retrieveReglement(savedReglement.getIdReglement());
+			
+		} 
+	
+	
+	@Test
+	public void testRetrieveAllReglements() throws ParseException {
+		List<Reglement> reglements = reglementService.retrieveAllReglements();
+		int expected = reglements.size();
+		Reglement r = new Reglement();
+		Reglement reglement = reglementService.addReglement(r);
+		assertEquals(expected + 1, reglementService.retrieveAllReglements().size());
+		reglementService.retrieveReglement(reglement.getIdReglement());
+
+	}
+	
+	
+	
+
+}
