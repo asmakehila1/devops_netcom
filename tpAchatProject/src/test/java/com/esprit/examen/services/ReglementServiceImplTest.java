5b76ea0015bfc32a974bb68b9fba70887196083f
levy20 <levy.bedi@esprit.tn>
2022-11-07 17:05:16 +0100

    update Junit test

--- tpAchatProject/src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
+++ tpAchatProject/src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
@@ -23,7 +23,7 @@ import java.text.ParseException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ReglementServicelmplTest {
+public class ReglementServiceImplTest {
 	@Autowired
 	IReglementService reglementService;
 	
