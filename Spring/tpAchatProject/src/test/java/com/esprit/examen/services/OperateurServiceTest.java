3dea03166d156578521026358f4aabfc20b26971
asmakehila1 <asma.kehila1@gmail.com>
2022-11-10 06:42:17 +0100

    Adding Junit tests

---
+++ Spring/tpAchatProject/src/test/java/com/esprit/examen/services/OperateurServiceTest.java
@@ -0,0 +1,100 @@
+package com.esprit.examen.services;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import com.esprit.examen.entities.Operateur;
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Slf4j
+@TestMethodOrder(OrderAnnotation.class)
+
+public class OperateurServiceTest {
+	@Autowired
+	IOperateurService OperateurService;
+	
+	@Test
+	@Order(1)
+	public void testAddOperateur()
+	{
+		List<Operateur> operateurs = OperateurService.retrieveAllOperateurs();
+		Operateur o = new Operateur();
+		o = new Operateur("nom_op1", "prenom_op1", "op1_password", null);
+		Operateur savedOperateur = OperateurService.addOperateur(o);
+		assertTrue(savedOperateur.getNom().length() > 0);
+		assertTrue(savedOperateur.getPrenom().length() > 0);
+		assertTrue(PasswordValidator.isValid(savedOperateur.getPassword()));
+		OperateurService.deleteOperateur(savedOperateur.getIdOperateur());
+	}
+	
+
+
+
+	@Test
+	@Order(2)
+	public void  retrieveAllOperateurs()
+	{
+		Operateur op = new Operateur();
+		op = new Operateur("nom_op1", "prenom_op1", "op1_password", null);
+		OperateurService.addOperateur(op);
+		List<Operateur> operateurs = OperateurService.retrieveAllOperateurs();	
+		assertNotNull(operateurs);
+    	System.out.println("all operateur retrieved");
+
+	}
+	
+	@Test
+	@Order(3)
+	public void updateOperateur()
+	{
+		Operateur operateur = OperateurService.retrieveOperateur((long) 2);
+		assertNotNull(operateur);
+		
+		Operateur op = new Operateur() ;
+		op.setNom("op2-nom");
+		op.setPrenom("op2-prenom");
+		op.setPassword("op2-password");
+		Operateur savedOperateur = OperateurService.updateOperateur(operateur);
+		assertNotNull(savedOperateur);
+		
+	}
+	
+	@Test
+	@Order(4)
+	public void deleteOperateur() 
+	{
+		Operateur op = new Operateur("nom_op1", "prenom_op1", "op1_password", null);
+		Operateur savedOperateur = OperateurService.addOperateur(op);
+		assertNotNull(savedOperateur);
+		OperateurService.deleteOperateur(savedOperateur.getIdOperateur());
+		assertNull(OperateurService.retrieveOperateur(savedOperateur.getIdOperateur()));
+		
+	}
+	
+	
+
+}
---
+++ Spring/tpAchatProject/src/test/java/com/esprit/examen/services/SecteurActiviteServiceTest.java
@@ -0,0 +1,102 @@
+package com.esprit.examen.services;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.persistence.ManyToMany;
+
+import org.junit.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.esprit.examen.entities.Fournisseur;
+import com.esprit.examen.entities.SecteurActivite;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Slf4j
+@TestMethodOrder(OrderAnnotation.class)
+
+public class SecteurActiviteServiceTest {
+	@Autowired
+	ISecteurActiviteService SecteurActiviteService;
+	@Test
+	@Order(1)
+	public void testAddSecteurActivite()
+	{
+		List<SecteurActivite> secteurAcitivites = SecteurActiviteService.retrieveAllSecteurActivite();
+		SecteurActivite sa = new SecteurActivite();
+		sa = new SecteurActivite("sa01", "sa_libelle", null);
+		SecteurActivite savedSecteurAcitivites = SecteurActiviteService.addSecteurActivite(sa);
+		assertTrue(savedSecteurAcitivites.getLibelleSecteurActivite().length() > 0);
+		assertTrue(savedSecteurAcitivites.getCodeSecteurActivite().length() > 0);
+		SecteurActiviteService.deleteSecteurActivite(savedSecteurAcitivites.getIdSecteurActivite());
+	}
+	
+
+
+
+	@Test
+	@Order(2)
+	public void  retrieveAllSecteurActivite()
+	{
+		SecteurActivite sa = new SecteurActivite();
+		sa = new SecteurActivite("sa01", "sa1_libelle", null);
+		SecteurActiviteService.addSecteurActivite(sa);
+		List<SecteurActivite> SecteurActivites = SecteurActiviteService.retrieveAllSecteurActivite();	
+		assertNotNull(SecteurActivites);
+    	System.out.println("all SecteurActivite retrieved");
+
+	}
+	
+	@Test
+	@Order(3)
+	public void updateSecteurActivite()
+	{
+		SecteurActivite secteurActivite = SecteurActiviteService.retrieveSecteurActivite((long) 2);
+		assertNotNull(secteurActivite);
+		
+		SecteurActivite sa = new SecteurActivite() ;
+		sa.setCodeSecteurActivite("sa02");
+		sa.setLibelleSecteurActivite("sa2_libelle");
+		SecteurActivite savedSecteurActivite = SecteurActiviteService.updateSecteurActivite(secteurActivite);
+		assertNotNull(savedSecteurActivite);
+		
+	}
+	
+	@Test
+	@Order(4)
+	public void deleteSecteurActivite() 
+	{
+		SecteurActivite sa = new SecteurActivite("sa01", "sa1_libelle", null);
+		SecteurActivite savedSecteurActivite = SecteurActiviteService.addSecteurActivite(sa);
+		assertNotNull(savedSecteurActivite);
+		SecteurActiviteService.deleteSecteurActivite(savedSecteurActivite.getIdSecteurActivite());
+		assertNull(SecteurActiviteService.retrieveSecteurActivite(savedSecteurActivite.getIdSecteurActivite()));
+		
+	}
+	
+	
+
+}
